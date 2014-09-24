2874ee28e14d03c553bbe3d6e983b621f08e055d
Liam Connor <liamconnor@Liams-MacBook-Pro.local>
2014-09-24 09:15:43 -0400

    Changed to using AnData for now

--- ch_pipeline/calibration.py
+++ ch_pipeline/calibration.py
@@ -18,6 +18,7 @@ Tasks
 
 from caput import pipeline
 from caput import config
+from ch_util import anddata
 
 import gain_sol_transit as gsol # Temp until the eigensolver is in ch_util. 
 # Or should the gainsol code go here?
@@ -35,15 +36,21 @@ class PointSourceCalibration(pipeline.TaskBase):
 
         This is parallelized over frequency, right?
 
-        Should the linear interpolation go in here?
+        Should the linear interpolation go in here? I'll have 
+        one gain per feed per frequency per sidereal day. 
         """
-        ts = containers.TimeStream.from_acq_files(files)
+        #ts = containers.TimeStream.from_acq_files(files)
+        data = andata.AnData.from_acq_h5(files)
         # Need to select subset of this data
-        ts_xpol, ts_ypol = ts, ts # Need to figure out a way to select pols
+        data_xpol, data_ypol = data.vis, data.vis # Need to figure out a way to select pols
+        # Will this handle an MPIdataset? 
         
-        gain_arr_x = gsol.solve_gain(ts_xpol)
-        gain_arr_y = gsol.solve_gain(tx_ypol)
+        gain_arr_x = gsol.solve_gain(data_xpol)
+        gain_arr_y = gsol.solve_gain(data_ypol)
 
+        gain_sol = np.concatenate([gain_arr_x[np.newaxis], gain_arr_y[np.newaxis]])
+
+    return gain_sol
 
     def next(self, data):
 
