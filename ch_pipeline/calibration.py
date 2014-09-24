8d914ab3ae1c8522ff696b0f2de9fd53700496df
Liam Connor <liamconnor@Liams-MacBook-Pro.local>
2014-09-24 10:00:03 -0400

    Decided to include eigendecomp code in calibration.py

--- ch_pipeline/calibration.py
+++ ch_pipeline/calibration.py
@@ -19,9 +19,74 @@ Tasks
 from caput import pipeline
 from caput import config
 from ch_util import andata
+from ch_util import tools 
 
 import gain_sol_transit as gsol # Temp until the eigensolver is in ch_util. 
 # Or should the gainsol code go here?
+
+def gen_corr_matrix(data, nfeed, feed_loc=False):
+     """Generates Hermitian (nfeed, nfeed) correlation matrix from unique correlations
+
+     Parameters
+     ----------
+     data: (nfreq, ncorr, ntimes) np.complex128 arr
+          Visibility array to be decomposed
+     nfeed: int 
+          Number of feeds (duhhh)
+
+     Returns
+     -------
+     Hermitian correlation matrix
+     """
+
+     if not feed_loc:
+          feed_loc=range(nfeed)
+          
+     corr_mat = np.zeros((len(feed_loc), len(feed_loc)), np.complex128)
+
+     for ii in range(len(feed_loc)):
+          for jj in range(ii, len(feed_loc)):
+               corr_mat[ii, jj] = data[tools.cmap(feed_loc[ii], feed_loc[jj], nfeed)]
+               corr_mat[jj, ii] = np.conj(data[tools.cmap(feed_loc[ii], feed_loc[jj], nfeed)])
+
+     return corr_mat
+
+def solve_gain(data, nfeed, feed_loc=False):
+     """
+     Steps through each time/freq pixel, generates a Hermitian (nfeed,nfeed)
+     matrix and calculates gains from its largest eigenvector.
+
+     Parameters
+     ----------
+     data: (nfreq, ncorr, ntimes) np.complex128 arr
+          Visibility array to be decomposed
+     nfeed: int 
+          Number of feeds in configuration
+     feed_loc: list
+          Which feeds to include 
+
+     Returns
+     -------
+     gain_arr: (nfreq, nfeed, ntimes) np.complex128
+          Gain solution for each feed, time, and frequency
+
+     """
+     if not feed_loc:
+          feed_loc=range(nfeed)     
+
+     gain_arr = np.zeros([data.shape[0], len(feed_loc), data.shape[-1]], np.complex128)
+
+     for nu in range(data.shape[0]):
+          if (nu%64)==0:
+               print "Freq %d" % nu
+          for tt in range(data.shape[-1]):
+               corr_arr = gen_corr_matrix(data[nu, :, tt], nfeed, feed_loc=feed_loc)
+               corr_arr[np.diag_indices(len(corr_arr))] = 0.0
+
+               evl, evec = np.linalg.eigh(corr_arr) 
+               gain_arr[nu, :, tt] = evl[-1]**0.5 * evec[:, -1]
+
+     return gain_arr
 
 
 class PointSourceCalibration(pipeline.TaskBase):
