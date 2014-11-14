bfc71987690f39d4edc0bc12b63dba5d1b70b5b7
Richard Shaw <jrs65@cita.utoronto.ca>
2014-11-14 11:22:01 -0500

    Fixed up version of the regridding code.

--- ch_pipeline/_regrid_work.pyx
+++ ch_pipeline/_regrid_work.pyx
@@ -1,30 +1,39 @@
-
+from cython.parallel import prange, parallel
 cimport cython
 
 import numpy as np
 cimport numpy as np
 
+cdef inline int int_max(int a, int b) nogil: return a if a >= b else b
+
+
+# A routine for quickly calculating the noise part of the banded
+# covariance matrix for the Wiener filter.
 @cython.boundscheck(False)
 cpdef _band_wiener_covariance(double [:, ::1] Rn, double [::1] Ni, int bw):
 
     cdef double [:, ::1] Ci = np.zeros((bw+1, Rn.shape[0]), dtype=np.float64)
-    Ci[:] = 0.0
 
     cdef unsigned int N, M
-    cdef unsigned int alpha, beta, betap, j
+    cdef unsigned int alpha, beta, betap, j, alpha_start
 
     cdef double t
 
     N = Rn.shape[0]
     M = Rn.shape[1]
 
-    with nogil:
-        for alpha in range(bw+1):
-            for beta in range(bw - alpha, N):
-                betap = alpha + beta - bw
-                t = 0.0
-                for j in range(M):
-                    t += Rn[betap, j] * Rn[beta, j] * Ni[j]
-                Ci[alpha, beta] = t
+    # Loop over the band array indices to generate each one (opposite
+    # order for faster parallelisation)
+    for beta in prange(N, nogil=True):
+
+        # Calculate alphas to start at
+        alpha_start = int_max(0, bw - beta)
+        
+        for alpha in range(alpha_start, bw+1):
+            betap = alpha + beta - bw
+            t = 0.0
+            for j in range(M):
+                t = t + Rn[betap, j] * Rn[beta, j] * Ni[j]
+            Ci[alpha, beta] = t
 
     return np.asarray(Ci)
