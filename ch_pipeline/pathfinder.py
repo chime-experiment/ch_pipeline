e765dd1e586bfbfe070671ad4c24aaba7acf8aec
Richard Shaw <jrs65@cita.utoronto.ca>
2015-01-13 13:37:20 -0500

    Initial map making code.  - A first pass at a map maker (can make dirty, ML and Wiener maps)  - Simulation code for testing out the map maker.  - New containers for holding maps and m-modes.  - Telescope class for modeling the CHIME pathfinder.

---
+++ ch_pipeline/pathfinder.py
@@ -0,0 +1,257 @@
+"""
+==========================================================
+Pathfinder Telescope Model (:mod:`ch_pipeline.pathfinder`)
+==========================================================
+
+.. currentmodule:: ch_pipeline.pathfinder
+
+A model for the CHIME Pathfinder telescope. This attempts to query the
+configuration db (:mod:`~ch_analysis.pathfinder.configdb`) for the details of
+the feeds and their positions.
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+
+    CHIMEPathfinder
+"""
+
+import numpy as np
+
+from drift.core import telescope
+from drift.telescope import cylbeam
+
+from drift.util import config, mpiutil
+from ch_util import ephemeris
+
+
+class CHIMEPathfinder(telescope.PolarisedTelescope):
+    """Model telescope for the CHIME Pathfinder.
+
+    Attributes
+    ----------
+    layout : datetime or int
+        Specify which layout to use.
+    correlator : string
+        Restrict to a specific correlator.
+    skip_non_chime : boolean
+        Ignore non CHIME feeds in the BeamTransfers.
+    """
+
+    layout = config.Property(default=None)
+    correlator = config.Property(proptype=str, default=None)
+    skip_non_chime = config.Property(proptype=bool, default=True)
+
+    num_cylinders = 2
+    cylinder_width = 20.0
+    cylinder_spacing = 22.0
+    cylinder_length = 40.0
+
+    rotation_angle = 1.986  # Anti-clockwise looking at the ground (degrees)
+
+    zenith = telescope.latlon_to_sphpol([ephemeris.CHIMELATITUDE, 0.0])
+    # zenith = telescope.latlon_to_sphpol([0.0, 0.0])
+#                                         ephemeris.CHIMELONGITUDE])
+
+    auto_correlations = True
+
+    _pickle_keys = ['_feeds']
+
+    ## Tweak the following two properties to change the beam width
+    @property
+    def fwhm_e(self):
+        """Full width half max of the E-plane antenna beam."""
+        return 2.0 * np.pi / 3.0 * 0.7
+
+    @property
+    def fwhm_h(self):
+        """Full width half max of the H-plane antenna beam."""
+        return 2.0 * np.pi / 3.0 * 1.2
+
+    ## u-width property override
+    @property
+    def u_width(self):
+        return self.cylinder_width
+
+    ## v-width property override
+    @property
+    def v_width(self):
+        return 1.0
+
+    @property
+    def feeds(self):
+        return list(self._feeds)
+
+    def __init__(self, feeds=None):
+
+        self._feeds = feeds
+
+    def load_layout(self):
+
+        if self.layout is None:
+            raise Exception("Layout attributes not set.")
+
+        from ch_util import tools
+
+        # Fetch feed layout from database
+        feeds = tools.get_correlator_inputs(self.layout, self.correlator)
+
+        if mpiutil.size > 1:
+            feeds = mpiutil.world.bcast(feeds, root=0)
+
+        if not self.skip_non_chime:
+            raise Exception("Not supported.")
+
+        # Filter and sort feeds
+        feeds = [ feed for feed in feeds if isinstance(feed, tools.CHIMEAntenna) ]
+        feeds = sorted(feeds, key=lambda feed: feed.input_sn)
+
+        self._feeds = feeds
+
+    @classmethod
+    def from_layout(cls, layout, correlator=None, skip=True):
+        """Create a Pathfinder telescope description for the specified layout.
+
+        Parameters
+        ----------
+        layout : integer or datetime
+            Layout id number (corresponding to one in the database), or a datetime.
+        correlator : string, optional
+            Name of the specific correlator. Needed to return a unique config in
+            some cases.
+        skip : boolean, optional
+            Whether to skip non-CHIME antennas. If False, leave them in but
+            set them to infinite noise (unsupported at the moment).
+
+        Returns
+        -------
+        tel : CHIMEPathfinder
+        """
+
+        pf = cls()
+
+        pf.layout = layout
+        pf.correlator = correlator
+        pf.skip_non_chime = skip
+        pf.load_layout()
+
+        return pf
+
+    def _finalise_config(self):
+        # Override base method to implement automatic loading of layout when
+        # configuring from YAML.
+
+        if self.layout is not None:
+            self.load_layout()
+
+    def _sort_pairs(self):
+        ## Reimplemented sort pairs to ensure that returned array is in
+        ## channel order.
+
+        # Create mask of included pairs, that are not conjugated
+        tmask = np.logical_and(self._feedmask, np.logical_not(self._feedconj))
+        uniq = telescope._get_indices(self._feedmap, mask=tmask)
+
+        fi, fj = uniq[:, 0], uniq[:, 1]
+
+        # Fetch keys by which to sort (lexicographically)
+        ci = fi
+        cj = fj
+
+        ## Sort by constructing a numpy array with the keys as fields, and use
+        ## np.argsort to get the indices
+
+        # Create array of keys to sort
+        dt = np.dtype('i4,i4')
+        sort_arr = np.zeros(fi.size, dtype=dt)
+        sort_arr['f0'] = ci
+        sort_arr['f1'] = cj
+
+        # Get map which sorts
+        sort_ind = np.argsort(sort_arr)
+
+        # Invert mapping
+        tmp_sort_ind = sort_ind.copy()
+        sort_ind[tmp_sort_ind] = np.arange(sort_ind.size)
+
+        # Remap feedmap entries
+        fm_copy = self._feedmap.copy()
+        wmask = np.where(self._feedmask)
+        fm_copy[wmask] = sort_ind[self._feedmap[wmask]]
+
+        self._feedmap = fm_copy
+
+    def _make_ew(self):
+        ## Reimplemented to make sure entries we always pick the upper
+        ## triangle (and do not reorder to make EW baselines)
+        return
+
+    @property
+    def feedpositions(self):
+        """The set of feed positions on *all* cylinders.
+
+        This is constructd for the given layout and includes all rotations of
+        the cylinder axis.
+
+        Returns
+        -------
+        feedpositions : np.ndarray
+            The positions in the telescope plane of the receivers. Packed as
+            [[u1, v1], [u2, v2], ...].
+        """
+
+        # Fetch cylinder relative positions
+        pos_x = np.array([ f.cyl for f in self._feeds ]) * self.cylinder_spacing
+        pos_y = 20.0 - np.array([ f.pos for f in self._feeds ])  # Turn into distance increasing from South to North.
+
+        # Combine to get set of vector positions
+        pos = np.vstack([pos_x, pos_y])
+
+        # Apply cylinder rotation
+        t = np.radians(self.rotation_angle)
+        c, s = np.cos(t), np.sin(t)
+        rotmatrix = np.array([[c, -s], [s, c]])
+        pos = np.dot(rotmatrix, pos)
+
+        return pos.T  # Transpose to get into correct shape
+
+    @property
+    def channels(self):
+        ## Return channel numbers. Currently the same as beamclass but might change.
+        return np.array([ f.channel for f in self._feeds ])
+
+    @property
+    def beamclass(self):
+        ## Make beam class just channel number.
+        return np.arange(len(self.feeds))
+
+    def beam(self, feed, freq):
+        ## Fetch beam parameters out of config database.
+
+        feed_obj = self.feeds[feed]
+
+        # Get the beam rotation parameters.
+        yaw = self.rotation_angle
+        pitch = 0.0
+        roll = 0.0
+
+        rot = np.radians([yaw, pitch, roll])
+
+        if feed_obj is None:
+            raise Exception("Craziness. The requested feed doesn't seem to exist.")
+
+        # We can only support feeds angled parallel or perp to the cylinder
+        # axis. Check for these and throw exception for anything else.
+        if feed_obj.pol == "N" or feed_obj.pol == "S":
+            return cylbeam.beam_y(self._angpos, self.zenith,
+                                  self.cylinder_width / self.wavelengths[freq],
+                                  self.fwhm_e, self.fwhm_h, rot=rot)
+        elif feed_obj.pol == "E" or feed_obj.pol == "W":
+            return cylbeam.beam_x(self._angpos, self.zenith,
+                                  self.cylinder_width / self.wavelengths[freq],
+                                  self.fwhm_e, self.fwhm_h, rot=rot)
+        else:
+            raise Exception("Polarisation not supported.")
+
---
+++ ch_pipeline/simulate.py
@@ -0,0 +1,315 @@
+"""
+=====================================================
+Timestreaam Simulation (:mod:`~ch_pipeline.simulate`)
+=====================================================
+
+.. currentmodule:: ch_pipeline.simulate
+
+Tasks for simulating timestream data.
+
+Tasks
+=====
+
+.. autosummary::
+    :toctree: generated/
+
+    LoadBeamTransfer
+    SimulateSidereal
+    DayMask
+    Unroll
+"""
+
+import h5py
+import numpy as np
+
+from cora.util import hputil
+from caput import mpiutil, pipeline, config, mpidataset
+
+from ch_util import tools
+from ch_analysis.map import sidereal
+
+from . import containers
+
+
+class LoadBeamTransfer(pipeline.TaskBase):
+
+    product_directory = config.Property(proptype=str)
+
+    def setup(self):
+        """Load the beam transfer matrices.
+
+        Returns
+        -------
+        tel : TransitTelescope
+            Object describing the telescope.
+        bt : BeamTransfer
+            BeamTransfer manager.
+        """
+
+        from drift.core import beamtransfer
+
+        bt = beamtransfer.BeamTransfer(self.product_directory)
+
+        tel = bt.telescope
+
+        return tel, bt
+
+
+class SimulateSidereal(pipeline.TaskBase):
+    """Create a simulated timestream.
+
+    Attributes
+    ----------
+    maps : list
+        List of map filenames. The sum of these form the simulated sky.
+    ndays : int, optional
+        Number of days of observation. Setting `ndays = None` (default) uses
+        the default stored in the telescope object; `ndays = 0`, assumes the
+        observation time is infinite so that the noise is zero.
+    seed : integer, optional
+        Set the random seed used for the noise simulations. Default (None) is
+        to choose a random seed.
+    """
+    maps = config.Property(proptype=list)
+    ndays = config.Property(proptype=int, default=0)
+    seed = config.Property(proptype=int, default=None)
+
+    done = False
+
+    def setup(self, telescope, beamtransfer):
+        """Setup the simulation.
+
+        Parameters
+        ----------
+        tel : TransitTelescope
+            Telescope object.
+        bt : BeamTransfer
+            Beam Transfer maanger.
+        """
+        self.beamtransfer = beamtransfer
+        self.telescope = telescope
+
+    def next(self):
+        """Simulate a SiderealStream
+
+        Returns
+        -------
+        ss : SiderealStream
+            Stacked sidereal day.
+        """
+
+        if self.done:
+            raise pipeline.PipelineStopIteration
+
+        ## Read in telescope system
+        bt = self.beamtransfer
+        tel = self.telescope
+
+        lmax = tel.lmax
+        mmax = tel.mmax
+        nfreq = tel.nfreq
+        npol = tel.num_pol_sky
+
+        lfreq, sfreq, efreq = mpiutil.split_local(nfreq)
+        #local_freq = range(sfreq, efreq)
+
+        lm, sm, em = mpiutil.split_local(mmax + 1)
+
+        # Set the minimum resolution required for the sky.
+        ntime = 2*mmax+1
+
+        col_vis = np.zeros((tel.npairs, lfreq, ntime), dtype=np.complex128)
+
+        ## If we want to add maps use the m-mode formalism to project a skymap
+        ## into visibility space.
+
+        # Load file to find out the map shapes.
+        with h5py.File(self.maps[0], 'r') as f:
+            mapshape = f['map'].shape
+
+        if lfreq > 0:
+
+            # Allocate array to store the local frequencies
+            row_map = np.zeros((lfreq,) + mapshape[1:], dtype=np.float64)
+
+            # Read in and sum up the local frequencies of the supplied maps.
+            for mapfile in self.maps:
+                with h5py.File(mapfile, 'r') as f:
+                    row_map += f['map'][sfreq:efreq]
+
+            # Calculate the alm's for the local sections
+            row_alm = hputil.sphtrans_sky(row_map, lmax=lmax).reshape((lfreq, npol * (lmax+1), lmax+1))
+
+        else:
+            row_alm = np.zeros((lfreq, npol * (lmax+1), lmax+1), dtype=np.complex128)
+
+        # Perform the transposition to distribute different m's across processes. Neat
+        # tip, putting a shorter value for the number of columns, trims the array at
+        # the same time
+        col_alm = mpiutil.transpose_blocks(row_alm, (nfreq, npol * (lmax+1), mmax+1))
+
+        # Transpose and reshape to shift m index first.
+        col_alm = np.transpose(col_alm, (2, 0, 1)).reshape(lm, nfreq, npol, lmax+1)
+
+        # Create storage for visibility data
+        vis_data = np.zeros((lm, nfreq, bt.ntel), dtype=np.complex128)
+
+        # Iterate over m's local to this process and generate the corresponding
+        # visibilities
+        for mp, mi in enumerate(range(sm, em)):
+            vis_data[mp] = bt.project_vector_sky_to_telescope(mi, col_alm[mp])
+
+        # Rearrange axes such that frequency is last (as we want to divide
+        # frequencies across processors)
+        row_vis = vis_data.transpose((0, 2, 1))
+
+        # Parallel transpose to get all m's back onto the same processor
+        col_vis_tmp = mpiutil.transpose_blocks(row_vis, ((mmax+1), bt.ntel, nfreq))
+        col_vis_tmp = col_vis_tmp.reshape(mmax + 1, 2, tel.npairs, lfreq)
+
+        # Transpose the local section to make the m's the last axis and unwrap the
+        # positive and negative m at the same time.
+        col_vis[..., 0] = col_vis_tmp[0, 0]
+        for mi in range(1, mmax+1):
+            col_vis[...,  mi] = col_vis_tmp[mi, 0]
+            col_vis[..., -mi] = col_vis_tmp[mi, 1].conj()  # Conjugate only (not (-1)**m - see paper)
+
+        del col_vis_tmp
+
+        ## If we're simulating noise, create a realisation and add it to col_vis
+        if self.ndays > 0:
+
+            # Fetch the noise powerspectrum
+            noise_ps = tel.noisepower(np.arange(tel.npairs)[:, np.newaxis],
+                                      np.arange(sfreq, efreq)[np.newaxis, :],
+                                      ndays=self.ndays).reshape(tel.npairs, lfreq)[:, :, np.newaxis]
+
+            # Seed random number generator to give consistent noise
+            if self.seed is not None:
+                # Must include rank such that we don't have massive power deficit from correlated noise
+                np.random.seed(self.seed + mpiutil.rank)
+
+            # Create and weight complex noise coefficients
+            noise_vis = (np.array([1.0, 1.0J]) * np.random.standard_normal(col_vis.shape + (2,))).sum(axis=-1)
+            noise_vis *= (noise_ps / 2.0)**0.5
+
+            # Reset RNG
+            if self.seed is not None:
+                np.random.seed()
+
+            # Add into main noise sims
+            col_vis += noise_vis
+
+            del noise_vis
+
+        # Fourier transform m-modes back to get timestream.
+        vis_stream = np.fft.ifft(col_vis, axis=-1) * ntime
+        vis_stream = vis_stream.reshape(tel.npairs, lfreq, ntime)
+
+        sstream = containers.SiderealStream(ntime, nfreq, tel.npairs)
+        sstream['vis'][:] = vis_stream.transpose((1, 0, 2))
+
+        self.done = True
+
+        return sstream
+
+
+class DayMask(pipeline.TaskBase):
+    """Crudely simulate a masking out of the daytime data.
+
+    Attributes
+    ----------
+    start, end : float
+        Start and end of masked out region.
+    width : float
+        Use a smooth transition of given width between the fully masked and
+        unmasked data. This is interior to the region marked by start and end.
+    """
+
+    start = config.Property(proptype=float, default=90.0)
+    end = config.Property(proptype=float, default=270.0)
+
+    width = config.Property(proptype=float, default=60.0)
+
+    def next(self, sstream):
+        """Apply a day time mask.
+
+        Parameters
+        ----------
+        sstream : containers.SiderealStream
+            Unmasked sidereal stack.
+
+        Returns
+        -------
+        mstream : containers.SiderealStream
+            Masked sidereal stream.
+        """
+
+        ra = sstream.ra
+
+        # Crudely mask the on and off regions
+        mask = np.logical_or(ra < self.start, ra > self.end).astype(np.float64)
+
+        # Put in the transition at the start of the day
+        mask = np.where(np.logical_and(ra > self.start, ra < self.start + self.width),
+                        0.5 * (1 + np.cos(np.pi * ((ra - self.start) / self.width))),
+                        mask)
+
+        # Put the transition at the end of the day
+        mask = np.where(np.logical_and(ra > self.end - self.width, ra < self.end),
+                        0.5 * (1 + np.cos(np.pi * ((ra - self.end) / self.width))),
+                        mask)
+
+        # Apply the mask to the data
+        sstream.vis[:] *= mask
+
+        return sstream
+
+
+class Unroll(pipeline.TaskBase):
+    """Unroll a sidereal stack.
+
+    Not really tested at the moment.
+
+    Parameters
+    ----------
+    start_time, end_time : float
+        Start and end UNIX times of the timestream to simulate.
+    int_time : float
+        Integration time in seconds.
+
+    """
+
+    start_time = config.Property(proptype=float)
+    end_time = config.Property(proptype=float)
+
+    int_time = config.Property(proptype=float)
+
+    def setup(self, telescope):
+        self.telescope = telescope
+
+    def next(self, sstream):
+
+        tel = self.telescope
+
+        # Use Kiyo's code to unroll the timestream in time.
+        sunroll, times = sidereal.unroll_stream(sstream.ra, sstream.data, self.start_time, self.end_time, self.int_time)
+        sunroll = mpidataset.MPIArray.wrap(sunroll, 0, comm=sstream.comm)
+
+        allpair = tel.nfeed * (tel.nfeed + 1) / 2
+
+        tstream = containers.TimeStream(times, tel.nfreq, allpair)
+
+        # Iterate over all feed pairs and work out which is the correct index in the sidereal stack.
+        for fi in range(tel.nfeed):
+            for fj in range(fi, tel.nfeed):
+                pair_ind = tools.fmap(fi, fj, tel.nfeed)
+                upair_ind = tel.feedmap[fi, fj]
+
+                # Skip if upair is not a proper index (has been masked)
+                if upair_ind < 0:
+                    continue
+
+                tstream.data[:, pair_ind] = sunroll[:, upair_ind]
+
+        return sstream
