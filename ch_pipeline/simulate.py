96aef975d05e5bcdc1a33bd52f98d561aabc962b
Richard Shaw <jrs65@cita.utoronto.ca>
2015-07-08 12:15:10 -0700

    Continued major rewrite to get map making code working.xs

--- ch_pipeline/simulate.py
+++ ch_pipeline/simulate.py
@@ -19,19 +19,55 @@ Tasks
     Unroll
 """
 
-import h5py
 import numpy as np
 
 from cora.util import hputil
-from caput import mpiutil, pipeline, config, mpidataset
+from caput import mpiutil, pipeline, config, memh5, mpiarray
 
 from ch_util import tools
 from ch_analysis.map import sidereal
 
-from . import containers
+from . import containers, task
+
+
+class LoadBasicCont(pipeline.TaskBase):
+    """Load a series of files from disk and pass them through the pipeline.
+
+    Uses the ability of :class:`memh5.BasicCont` to return the data in the class
+    it was saved in.
+
+    Attributes
+    ----------
+    files : list
+        A list containing the paths of files to load.
+    """
+
+    files = config.Property(proptype=list)
+
+    def next(self):
+        """Load the given files in turn and pass on.
+
+        Returns
+        -------
+        cont : subclass of `memh5.BasicCont`
+        """
+
+        # Fetch and remove the first item in the list
+        file = self.files.pop(0)
+
+        cont = memh5.BasicCont.from_file(file, distributed=True)
+
+        return cont
 
 
 class LoadBeamTransfer(pipeline.TaskBase):
+    """Loads a beam transfer manager from disk.
+
+    Attributes
+    ----------
+    product_directory : str
+        Path to the saved Beam Transfer products.
+    """
 
     product_directory = config.Property(proptype=str)
 
@@ -44,18 +80,29 @@ class LoadBeamTransfer(pipeline.TaskBase):
             Object describing the telescope.
         bt : BeamTransfer
             BeamTransfer manager.
+        feed_info : list, optional
+            Optional list providing additional information about each feed.
         """
 
+        import os
+
         from drift.core import beamtransfer
+
+        if not os.path.exists(self.product_directory):
+            raise RuntimeError('BeamTransfers do not exist.')
 
         bt = beamtransfer.BeamTransfer(self.product_directory)
 
         tel = bt.telescope
 
-        return tel, bt, tel.feeds
+        try:
+            feed_info = tel.feed_info
+            return tel, bt, feed_info
+        except AttributeError:
+            return tel, bt
 
 
-class SimulateSidereal(pipeline.TaskBase):
+class SimulateSidereal(task.SingleTask):
     """Create a simulated timestream.
 
     Attributes
@@ -90,7 +137,7 @@ class SimulateSidereal(pipeline.TaskBase):
         self.beamtransfer = beamtransfer
         self.telescope = telescope
 
-    def next(self):
+    def process(self):
         """Simulate a SiderealStream
 
         Returns
@@ -104,7 +151,7 @@ class SimulateSidereal(pipeline.TaskBase):
         if self.done:
             raise pipeline.PipelineStopIteration
 
-        ## Read in telescope system
+        # Read in telescope system
         bt = self.beamtransfer
         tel = self.telescope
 
@@ -114,31 +161,35 @@ class SimulateSidereal(pipeline.TaskBase):
         npol = tel.num_pol_sky
 
         lfreq, sfreq, efreq = mpiutil.split_local(nfreq)
-        #local_freq = range(sfreq, efreq)
+        freqmap = None
 
         lm, sm, em = mpiutil.split_local(mmax + 1)
 
         # Set the minimum resolution required for the sky.
         ntime = 2*mmax+1
 
-        col_vis = np.zeros((tel.npairs, lfreq, ntime), dtype=np.complex128)
-
-        ## If we want to add maps use the m-mode formalism to project a skymap
-        ## into visibility space.
-
-        # Load file to find out the map shapes.
-        with h5py.File(self.maps[0], 'r') as f:
-            mapshape = f['map'].shape
+        # If we want to add maps use the m-mode formalism to project a skymap
+        # into visibility space
 
         if lfreq > 0:
 
             # Allocate array to store the local frequencies
-            row_map = np.zeros((lfreq,) + mapshape[1:], dtype=np.float64)
+            row_map = None
 
             # Read in and sum up the local frequencies of the supplied maps.
             for mapfile in self.maps:
-                with h5py.File(mapfile, 'r') as f:
-                    row_map += f['map'][sfreq:efreq]
+
+                mc = containers.Map.from_file(mapfile, distributed=True)
+                mc.redistribute('freq')
+                freqmap = mc.index_map['freq'][:]
+
+                if (tel.frequencies != freqmap['centre']).all():
+                    raise RuntimeError('Frequencies in map file (%s) do not match those in Beam Transfers.' % mapfile)
+
+                if row_map is None:
+                    row_map = mc.map[:]
+                else:
+                    row_map += mc.map[:]
 
             # Calculate the alm's for the local sections
             row_alm = hputil.sphtrans_sky(row_map, lmax=lmax).reshape((lfreq, npol * (lmax+1), lmax+1))
@@ -146,32 +197,39 @@ class SimulateSidereal(pipeline.TaskBase):
         else:
             row_alm = np.zeros((lfreq, npol * (lmax+1), lmax+1), dtype=np.complex128)
 
+        # Trim off excess m's and wrap into MPIArray
+        row_alm = row_alm[..., :(mmax+1)]
+        row_alm = mpiarray.MPIArray.wrap(row_alm, axis=0)
+
         # Perform the transposition to distribute different m's across processes. Neat
         # tip, putting a shorter value for the number of columns, trims the array at
         # the same time
-        col_alm = mpiutil.transpose_blocks(row_alm, (nfreq, npol * (lmax+1), mmax+1))
+        col_alm = row_alm.redistribute(axis=2)
 
         # Transpose and reshape to shift m index first.
-        col_alm = np.transpose(col_alm, (2, 0, 1)).reshape(lm, nfreq, npol, lmax+1)
+        col_alm = col_alm.transpose((2, 0, 1)).reshape((None, nfreq, npol, lmax+1))
 
         # Create storage for visibility data
-        vis_data = np.zeros((lm, nfreq, bt.ntel), dtype=np.complex128)
+        vis_data = mpiarray.MPIArray((lm, nfreq, bt.ntel), axis=0, dtype=np.complex128)
+        vis_data[:] = 0.0
 
         # Iterate over m's local to this process and generate the corresponding
         # visibilities
-        for mp, mi in enumerate(range(sm, em)):
-            vis_data[mp] = bt.project_vector_sky_to_telescope(mi, col_alm[mp])
+        for mp, mi in vis_data.enumerate(axis=0):
+            vis_data[mp] = bt.project_vector_sky_to_telescope(mi, col_alm[mp].view(np.ndarray))
 
         # Rearrange axes such that frequency is last (as we want to divide
         # frequencies across processors)
         row_vis = vis_data.transpose((0, 2, 1))
 
         # Parallel transpose to get all m's back onto the same processor
-        col_vis_tmp = mpiutil.transpose_blocks(row_vis, ((mmax+1), bt.ntel, nfreq))
-        col_vis_tmp = col_vis_tmp.reshape(mmax + 1, 2, tel.npairs, lfreq)
+        col_vis_tmp = row_vis.redistribute(axis=2)
+        col_vis_tmp = col_vis_tmp.reshape((mmax + 1, 2, tel.npairs, None))
 
         # Transpose the local section to make the m's the last axis and unwrap the
         # positive and negative m at the same time.
+        col_vis = mpiarray.MPIArray((tel.npairs, nfreq, ntime), axis=1, dtype=np.complex128)
+        col_vis[:] = 0.0
         col_vis[..., 0] = col_vis_tmp[0, 0]
         for mi in range(1, mmax+1):
             col_vis[...,  mi] = col_vis_tmp[mi, 0]
@@ -179,52 +237,52 @@ class SimulateSidereal(pipeline.TaskBase):
 
         del col_vis_tmp
 
-        ## If we're simulating noise, create a realisation and add it to col_vis
-        if self.ndays > 0:
+        # If we're simulating noise, create a realisation and add it to col_vis
+        # if self.ndays > 0:
+        #
+        #     # Fetch the noise powerspectrum
+        #     noise_ps = tel.noisepower(np.arange(tel.npairs)[:, np.newaxis],
+        #                               np.arange(sfreq, efreq)[np.newaxis, :],
+        #                               ndays=self.ndays).reshape(tel.npairs, lfreq)[:, :, np.newaxis]
+        #
+        #     # Seed random number generator to give consistent noise
+        #     if self.seed is not None:
+        #         # Must include rank such that we don't have massive power deficit from correlated noise
+        #         np.random.seed(self.seed + mpiutil.rank)
+        #
+        #     # Create and weight complex noise coefficients
+        #     noise_vis = (np.array([1.0, 1.0J]) * np.random.standard_normal(col_vis.shape + (2,))).sum(axis=-1)
+        #     noise_vis *= (noise_ps / 2.0)**0.5
+        #
+        #     # Reset RNG
+        #     if self.seed is not None:
+        #         np.random.seed()
+        #
+        #     # Add into main noise sims
+        #     col_vis += noise_vis
+        #
+        #     del noise_vis
 
-            # Fetch the noise powerspectrum
-            noise_ps = tel.noisepower(np.arange(tel.npairs)[:, np.newaxis],
-                                      np.arange(sfreq, efreq)[np.newaxis, :],
-                                      ndays=self.ndays).reshape(tel.npairs, lfreq)[:, :, np.newaxis]
-
-            # Seed random number generator to give consistent noise
-            if self.seed is not None:
-                # Must include rank such that we don't have massive power deficit from correlated noise
-                np.random.seed(self.seed + mpiutil.rank)
-
-            # Create and weight complex noise coefficients
-            noise_vis = (np.array([1.0, 1.0J]) * np.random.standard_normal(col_vis.shape + (2,))).sum(axis=-1)
-            noise_vis *= (noise_ps / 2.0)**0.5
-
-            # Reset RNG
-            if self.seed is not None:
-                np.random.seed()
-
-            # Add into main noise sims
-            col_vis += noise_vis
-
-            del noise_vis
-
-        # Fourier transform m-modes back to get timestream.
+        # Fourier transform m-modes back to get final timestream.
         vis_stream = np.fft.ifft(col_vis, axis=-1) * ntime
-        vis_stream = vis_stream.reshape(tel.npairs, lfreq, ntime)
+        vis_stream = vis_stream.reshape((tel.npairs, lfreq, ntime))
         vis_stream = vis_stream.transpose((1, 0, 2)).copy()
 
-        # Construct frequency array
-        freq = np.zeros(nfreq, dtype=[('centre', np.float64), ('width', np.float64)])
-        freq['centre'][:] = tel.frequencies
-        freq['width'][:] = np.diff(tel.frequencies)[0]
+        # Try and fetch out the feed index and info from the telescope object.
+        try:
+            feed_index = tel.feed_index
+        except AttributeError:
+            feed_index = tel.nfeed
 
-        sstream = containers.SiderealStream(ntime, freq, tel.npairs)
-        sstream['vis'][:] = mpidataset.MPIArray.wrap(vis_stream, axis=0)
-
-        sstream._distributed['weight'] = mpidataset.MPIArray.wrap(np.ones(vis_stream.shape, dtype=np.float64), axis=0)
+        # Construct container and set visibility data
+        sstream = containers.SiderealStream(freq=freqmap, ra=ntime, input=feed_index,
+                                            prod=tel.uniquepairs, distributed=True, comm=mc.comm)
+        sstream.vis[:] = mpiarray.MPIArray.wrap(vis_stream, axis=0)
+        sstream.weight[:] = 1.0
 
         self.done = True
 
-        feeds = self.telescope.feeds
-
-        return sstream, feeds
+        return sstream
 
 
 class DayMask(pipeline.TaskBase):
@@ -278,11 +336,11 @@ class DayMask(pipeline.TaskBase):
 
         return sstream
 
+
 def _list_of_timeranges(dlist):
 
     if not isinstance(list, dlist):
         pass
-
 
 
 class Unroll(pipeline.TaskBase):
@@ -313,7 +371,7 @@ class Unroll(pipeline.TaskBase):
 
         # Use Kiyo's code to unroll the timestream in time.
         sunroll, times = sidereal.unroll_stream(sstream.ra, sstream.data, self.start_time, self.end_time, self.int_time)
-        sunroll = mpidataset.MPIArray.wrap(sunroll, 0, comm=sstream.comm)
+        sunroll = mpiarray.MPIArray.wrap(sunroll, 0, comm=sstream.comm)
 
         allpair = tel.nfeed * (tel.nfeed + 1) / 2
 
