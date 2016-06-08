81e50ccfd71e6105506f1bdf6199f311d0629b6e
Richard Shaw <jrs65@cita.utoronto.ca>
2016-06-07 21:36:22 -0700

    Major improvements to the pathfinder class.  - Reworked the handling of feeds such that non cylinder feeds now get    included in the index_map (but are still not calculated).  - Cleaned up various methods in there.
    Also:
     - Removed some old cruft in stream.py
     - Improved handling of random number seeding.

--- ch_pipeline/core/pathfinder.py
+++ ch_pipeline/core/pathfinder.py
@@ -31,6 +31,8 @@ from ch_util import ephemeris, tools
 class CHIMEPathfinder(telescope.PolarisedTelescope):
     """Model telescope for the CHIME Pathfinder.
 
+    This class currently uses a simple Gaussian model for the primary beams.
+
     Attributes
     ----------
     layout : datetime or int
@@ -52,12 +54,15 @@ class CHIMEPathfinder(telescope.PolarisedTelescope):
     channel_bin : int, optional
         Number of channels to bin together. Must exactly divide the total
         number. Default is 1.
+    input_sel : list, optional
+        Select a reduced set of feeds to use. Useful for generating small
+        subsets of the data.
     """
 
     # Configure which feeds and layout to use
     layout = config.Property(default=None)
     correlator = config.Property(proptype=str, default=None)
-    skip_non_chime = config.Property(proptype=bool, default=True)
+    skip_non_chime = config.Property(proptype=bool, default=False)
 
     # Redundancy settings
     redundant = config.Property(proptype=bool, default=False)
@@ -67,6 +72,9 @@ class CHIMEPathfinder(telescope.PolarisedTelescope):
     channel_start = config.Property(proptype=int, default=0)
     channel_end = config.Property(proptype=int, default=1024)
     channel_bin = config.Property(proptype=int, default=1)
+
+    # Input selection
+    input_sel = config.Property(proptype=list, default=None)
 
     # Fix base properties
     num_cylinders = 2
@@ -82,106 +90,15 @@ class CHIMEPathfinder(telescope.PolarisedTelescope):
 
     _pickle_keys = ['_feeds']
 
-    # Tweak the following two properties to change the beam width
-    @property
-    def fwhm_e(self):
-        """Full width half max of the E-plane antenna beam."""
-        return 2.0 * np.pi / 3.0 * 0.7
-
-    @property
-    def fwhm_h(self):
-        """Full width half max of the H-plane antenna beam."""
-        return 2.0 * np.pi / 3.0 * 1.2
-
-    # u-width property override
-    @property
-    def u_width(self):
-        return self.cylinder_width
-
-    # v-width property override
-    @property
-    def v_width(self):
-        return 1.0
-
-    def calculate_frequencies(self):
-        # Override to give support for specifying channels
-
-        if self.freq_use_channels:
-            basefreq = np.linspace(800.0, 400.0, 1024, endpoint=False)
-            tfreq = basefreq[self.channel_start:self.channel_end]
-
-            if len(tfreq) % self.channel_bin != 0:
-                raise Exception("Channel binning must exactly divide the total number of channels")
-
-            self._frequencies = tfreq.reshape(-1, self.channel_bin).mean(axis=-1)
-        else:
-            telescope.TransitTelescope.calculate_frequencies(self)
-
-    @property
-    def feeds(self):
-        return list(self._feeds)
-
-    @property
-    def feed_index(self):
-        feed_sn = [feed.input_sn for feed in self.feeds]
-
-        # Parse a serial number into sma and slot number
-        def parse_sn(sn):
-            import re
-
-            mo = re.match('(\w{6}\-\d{4})(\d{2})(\d{2})', sn)
-
-            try:
-                crate = mo.group(1)
-                slot = int(mo.group(2))
-                sma = int(mo.group(3))
-
-                return crate, slot, sma
-            except:
-                raise RuntimeError('Serial number %s does not match expected format.' % sn)
-
-        # Map a slot and SMA to channel id
-        def get_channel(slot, sma):
-            c = [None, 80, 16, 64, 0, 208, 144, 192, 128, 240, 176, 224, 160, 112, 48, 96, 32]
-            channel = c[slot] + sma if slot > 0 else sma
-            return channel
-
-        channels = [get_channel(*(parse_sn(sn)[1:])) for sn in feed_sn]
-
-        from ch_util import andata
-
-        return andata._generate_input_map(feed_sn, channels)
-
-    @property
-    def feed_info(self):
-        return self.feeds
+    #
+    # === Initialisation routines ===
+    #
 
     def __init__(self, feeds=None):
-
-        self._feeds = feeds
-
-    def load_layout(self):
-
-        if self.layout is None:
-            raise Exception("Layout attributes not set.")
-
-        # Fetch feed layout from database
-        feeds = tools.get_correlator_inputs(self.layout, self.correlator)
-
-        if mpiutil.size > 1:
-            feeds = mpiutil.world.bcast(feeds, root=0)
-
-        if not self.skip_non_chime:
-            raise Exception("Not supported.")
-
-        # Filter and sort feeds
-        feeds = [ feed for feed in feeds if isinstance(feed, tools.CHIMEAntenna) ]
-        feeds = sorted(feeds, key=lambda feed: feed.input_sn)
-
         self._feeds = feeds
 
     @classmethod
-    def from_layout(cls, layout, correlator=None, skip=True):
+    def from_layout(cls, layout, correlator=None, skip=False):
         """Create a Pathfinder telescope description for the specified layout.
 
         Parameters
@@ -205,9 +122,29 @@ class CHIMEPathfinder(telescope.PolarisedTelescope):
         pf.layout = layout
         pf.correlator = correlator
         pf.skip_non_chime = skip
-        pf.load_layout()
+        pf._load_layout()
 
         return pf
+
+    def _load_layout(self):
+        """Load the Pathfinder layout from the database.
+
+        Generally this routine shouldn't be called directly. Use
+        :method:`CHIMEPathfinder.from_layout` or configure from a YAML file.
+        """
+        if self.layout is None:
+            raise Exception("Layout attributes not set.")
+
+        # Fetch feed layout from database
+        feeds = tools.get_correlator_inputs(self.layout, self.correlator)
+
+        if mpiutil.size > 1:
+            feeds = mpiutil.world.bcast(feeds, root=0)
+
+        if self.skip_non_chime:
+            raise Exception("Not supported.")
+
+        self._feeds = feeds
 
     def _finalise_config(self):
         # Override base method to implement automatic loading of layout when
@@ -215,28 +152,190 @@ class CHIMEPathfinder(telescope.PolarisedTelescope):
 
         if self.layout is not None:
             print "Loading layout: %s" % str(self.layout)
-            self.load_layout()
+            self._load_layout()
+
+    #
+    # === Redefine properties of the base class ===
+    #
+
+    # Tweak the following two properties to change the beam width
+    @property
+    def fwhm_e(self):
+        """Full width half max of the E-plane antenna beam."""
+        return 2.0 * np.pi / 3.0 * 0.7
+
+    @property
+    def fwhm_h(self):
+        """Full width half max of the H-plane antenna beam."""
+        return 2.0 * np.pi / 3.0 * 1.2
+
+    # Set the approximate uv feed sizes
+    @property
+    def u_width(self):
+        return self.cylinder_width
+
+    # v-width property override
+    @property
+    def v_width(self):
+        return 1.0
+
+    def calculate_frequencies(self):
+        """Override default version to give support for specifying by frequency
+        channel number.
+        """
+        if self.freq_use_channels:
+            # Calculate frequencies in terms of the channel numbers (assume the Pathfinder setup).
+            basefreq = np.linspace(800.0, 400.0, 1024, endpoint=False)
+            tfreq = basefreq[self.channel_start:self.channel_end]
+
+            if len(tfreq) % self.channel_bin != 0:
+                raise ValueError("Channel binning must exactly divide the total number of channels")
+
+            self._frequencies = tfreq.reshape(-1, self.channel_bin).mean(axis=-1)
+        else:
+            # Otherwise use the standard method
+            telescope.TransitTelescope.calculate_frequencies(self)
+
+    @property
+    def feeds(self):
+        """Return a description of the feeds as a list of :class:`tools.CorrInput` instances.
+        """
+
+        if self.input_sel is None:
+            feeds = self._feeds
+        else:
+            feeds = [ self._feeds[fi] for fi in self.input_sel ]
+
+        return feeds
+
+    @property
+    def input_index(self):
+        """An index_map describing the inputs known to the telescope. Useful
+        for generating synthetic datasets.
+        """
+        # Extract lists of channel ID and serial numbers
+        channels, feed_sn = zip(*[(feed.id, feed.input_sn) for feed in self.feeds])
+
+        # Create an input index map and return it.
+        from ch_util import andata
+        return andata._generate_input_map(feed_sn, channels)
+
+    @property
+    def feedpositions(self):
+        """The set of feed positions on *all* cylinders.
+
+        This is constructed for the given layout and includes all rotations of
+        the cylinder axis.
+
+        Returns
+        -------
+        feedpositions : np.ndarray
+            The positions in the telescope plane of the receivers. Packed as
+            [[u1, v1], [u2, v2], ...].
+        """
+
+        # Fetch cylinder relative positions
+        pos = tools.get_feed_positions(self.feeds, rotation=self.rotation_angle)
+
+        # The above routine returns NaNs for non CHIME feeds. This is a bit
+        # messy, so turn them into zeros.
+        pos = np.nan_to_num(pos)
+
+        return pos
+
+    @property
+    def beamclass(self):
+        """Beam class definition for the Pathfinder.
+
+        When `self.redundant` is set, the X-polarisation feeds gets `beamclass =
+        0`, and the Y-polarisation gets `beamclass = 1`. If `self.redundant` is
+        `False`, then the feeds are just give an increasing unique class. In
+        both cases, any other type of feed gets set to `-1` and should be
+        ignored.
+        """
+        # Make beam class just channel number.
+
+        def _feedclass(f):
+            if tools.is_chime_x(f):
+                return 0
+            if tools.is_chime_y(f):
+                return 1
+            return -1
+
+        if self.redundant:
+            return np.array([_feedclass(f) for f in self.feeds])
+        else:
+            beamclass = [
+                fi if tools.is_chime(feed) else -1
+                for fi, feed in enumerate(self.feeds)
+            ]
+            return np.array(beamclass)
+
+    #
+    # === Setup the primary beams ===
+    #
+
+    def beam(self, feed, freq):
+        """Primary beam implementation for the CHIME Pathfinder.
+
+        This only supports normal CHIME cylinder antennas. Asking for the beams
+        for other types of inputs will cause an exception to be thrown. The
+        beams from this routine are rotated by `self.rotation_angle` to account
+        for the Pathfinder rotation.
+        """
+        # # Fetch beam parameters out of config database.
+
+        feed_obj = self.feeds[feed]
+
+        # Check that feed exists and is a CHIME cylinder antenna
+        if feed_obj is None:
+            raise ValueError("Craziness. The requested feed doesn't seem to exist.")
+
+        if not tools.is_chime(feed_obj):
+            raise ValueError('Requested feed is not a CHIME antenna.')
+
+        # Get the beam rotation parameters.
+        yaw = -self.rotation_angle
+        pitch = 0.0
+        roll = 0.0
+
+        rot = np.radians([yaw, pitch, roll])
+
+        # We can only support feeds angled parallel or perp to the cylinder
+        # axis. Check for these and throw exception for anything else.
+        if tools.is_chime_y(feed_obj):
+            return cylbeam.beam_y(self._angpos, self.zenith,
+                                  self.cylinder_width / self.wavelengths[freq],
+                                  self.fwhm_e, self.fwhm_h, rot=rot)
+        elif tools.is_chime_x(feed_obj):
+            return cylbeam.beam_x(self._angpos, self.zenith,
+                                  self.cylinder_width / self.wavelengths[freq],
+                                  self.fwhm_e, self.fwhm_h, rot=rot)
+        else:
+            raise RuntimeError("Given polarisation (feed.pol=%s) not supported." % feed_obj.pol)
+
+    #
+    # === Override methods determining the feed pairs we should calculate ===
+    #
+    # These should probably get ported back into `driftscan` as options.
 
     def _sort_pairs(self):
-        # # Reimplemented sort pairs to ensure that returned array is in
-        # # channel order.
+        # Reimplemented sort pairs to ensure that returned array is in
+        # channel order.
 
         # Create mask of included pairs, that are not conjugated
         tmask = np.logical_and(self._feedmask, np.logical_not(self._feedconj))
         uniq = telescope._get_indices(self._feedmap, mask=tmask)
 
-        fi, fj = uniq[:, 0], uniq[:, 1]
-
-        # Fetch keys by which to sort (lexicographically)
-        ci = fi
-        cj = fj
+        # Get channel id for each feed in the pair, this will be used for the sort
+        ci, cj = np.array([(self.feeds[fi].id, self.feeds[fj].id) for fi, fj in uniq]).T
 
         # # Sort by constructing a numpy array with the keys as fields, and use
         # # np.argsort to get the indices
 
         # Create array of keys to sort
         dt = np.dtype('i4,i4')
-        sort_arr = np.zeros(fi.size, dtype=dt)
+        sort_arr = np.zeros(ci.size, dtype=dt)
         sort_arr['f0'] = ci
         sort_arr['f1'] = cj
 
@@ -260,70 +359,18 @@ class CHIMEPathfinder(telescope.PolarisedTelescope):
         if self.redundant:
             super(CHIMEPathfinder, self)._make_ew()
 
-    @property
-    def feedpositions(self):
-        """The set of feed positions on *all* cylinders.
+    def _unique_beams(self):
+        # Override to mask out any feed where the beamclass is less than zero.
+        # This is used to get exclude feeds which are not normal CHIME cylinder
+        # feeds
 
-        This is constructd for the given layout and includes all rotations of
-        the cylinder axis.
+        beam_map, beam_mask = telescope.TransitTelescope._unique_beams(self)
 
-        Returns
-        -------
-        feedpositions : np.ndarray
-            The positions in the telescope plane of the receivers. Packed as
-            [[u1, v1], [u2, v2], ...].
-        """
+        # Construct a mask including only the feeds where the beam class is
+        # greater than zero
+        bc_mask = (self.beamclass >= 0)
+        bc_mask = np.logical_and(bc_mask[:, np.newaxis], bc_mask[np.newaxis, :])
 
-        # Fetch cylinder relative positions
-        pos = tools.get_feed_positions(self.feeds, rotation=self.rotation_angle)
+        beam_mask = np.logical_and(beam_mask, bc_mask)
 
-        return pos  # Transpose to get into correct shape
-
-    @property
-    def channels(self):
-        # # Return channel numbers. Currently the same as beamclass but might change.
-        return np.array([ f.channel for f in self._feeds ])
-
-    @property
-    def beamclass(self):
-        # Make beam class just channel number.
-
-        def _feedclass(f):
-            if tools.is_chime_x(f):
-                return 0
-            if tools.is_chime_y(f):
-                return 1
-            return 2
-
-        if self.redundant:
-            return np.array([_feedclass(f) for f in self.feeds])
-        else:
-            return np.arange(len(self.feeds))
-
-    def beam(self, feed, freq):
-        # # Fetch beam parameters out of config database.
-
-        feed_obj = self.feeds[feed]
-
-        # Get the beam rotation parameters.
-        yaw = -self.rotation_angle
-        pitch = 0.0
-        roll = 0.0
-
-        rot = np.radians([yaw, pitch, roll])
-
-        if feed_obj is None:
-            raise Exception("Craziness. The requested feed doesn't seem to exist.")
-
-        # We can only support feeds angled parallel or perp to the cylinder
-        # axis. Check for these and throw exception for anything else.
-        if feed_obj.pol == "N" or feed_obj.pol == "S":
-            return cylbeam.beam_y(self._angpos, self.zenith,
-                                  self.cylinder_width / self.wavelengths[freq],
-                                  self.fwhm_e, self.fwhm_h, rot=rot)
-        elif feed_obj.pol == "E" or feed_obj.pol == "W":
-            return cylbeam.beam_x(self._angpos, self.zenith,
-                                  self.cylinder_width / self.wavelengths[freq],
-                                  self.fwhm_e, self.fwhm_h, rot=rot)
-        else:
-            raise Exception("Polarisation not supported.")
+        return beam_map, beam_mask
