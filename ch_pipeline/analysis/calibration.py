dafd978373eda214d381b82fd2846bb4211de051
Juan Mena <juan.menaparra@mail.mcgill.ca>
2017-04-17 14:23:54 -0400

    In calibration.py, the function ApplyGain now has the option to normalize the amplitude of the noise inj gains by the median amplitude over a data file. If this normalization is not done then the sidereal regriding task gets mad because the ni amplitudes cover a range of several orders of magnitude. By normalizing all the ni gains are of order 1

--- ch_pipeline/analysis/calibration.py
+++ ch_pipeline/analysis/calibration.py
@@ -75,6 +75,60 @@ def _extract_diagonal(utmat, axis=1):
 
     return diag_array
 
+def std_from_mad(a):
+    '''
+    Calculate standard deviation from Median absolute deviation of time (last) 
+    axis for array a. a is assumed to be REAL
+
+    Parameters
+    ----------
+    a : np.ndarray[freq, prod/input, time]
+        3d real array.
+
+    Returns
+    -------
+    a_std : np.ndarray[freq, prod/input]
+        Standard deviation along time (last) axis of a
+    '''
+
+    return np.median( abs(a - np.median(a, axis=-1)[..., np.newaxis]), axis=-1 )*1.4826
+
+def get_med_std(vis_array, frames_per_test=None):
+    '''Get median and standard deviation from MAD along time (last in general) axis.
+    vis_array has the nominal shape [freq, prod/input, time]. 
+    Finds the median and std for chunks of frames_per_test frames, and then takes 
+    median. frames_per_test must be even
+    
+    Parameters
+    ----------
+    vis_array : np.ndarray[freq, prod/input, time]
+        3d array.
+    frames_per_test: uint8
+        Number of samples per test. Must be even
+
+    Returns
+    -------
+    vis_array_med : np.ndarray[freq, prod/input]
+        Median along time (last) axis of a
+    vis_array_std : Standard deviation (from MAD) along time (last) axis of a
+    '''
+    
+    Nframes = vis_array.shape[-1]
+    if frames_per_test is None:
+        frames_per_test = Nframes
+        
+    Ntests = Nframes/frames_per_test #Number of tests per freq
+        
+    med = []
+    std = []
+    for i in range(Ntests):
+        v = vis_array[..., i*frames_per_test:(i+1)*frames_per_test]
+        v_diff = (v[..., ::2]-v[..., 1::2])/np.sqrt(2) # remove long time scale fluctuations
+        std.append(std_from_mad(v_diff)[..., np.newaxis])
+        med.append(np.median(v, axis=-1)[..., np.newaxis])
+        del v, v_diff
+                    
+    return np.median(np.concatenate(med, axis=-1), axis=-1), np.median(np.concatenate(std, axis=-1), axis=-1)
 
 def solve_gain(data, feeds=None, norm=None):
     """
@@ -803,11 +857,15 @@ class ApplyGain(task.SingleTask):
         with the inverse variance resulting from propagating the gain errors
         (assumes the current weight of the tstream is just the inverse variance 
         from the rad eqn).
+    normalize_gains: bool, optional
+        If True, the noise injection gains are normalized the median of the
+        gain magnitude along the time axis
     """
 
     inverse = config.Property(proptype=bool, default=True)
     update_weight = config.Property(proptype=bool, default=False)
     scale_weight_only = config.Property(proptype=bool, default=False)
+    normalize_gains = config.Property(proptype=float, default=False)
     smoothing_length = config.Property(proptype=float, default=None)
 
     def process(self, tstream, gain):
@@ -864,6 +922,9 @@ class ApplyGain(task.SingleTask):
 
             # Extract gain array
             gain_arr = gain.gain[:].view(np.ndarray) # If not viewed as ndarray get error when smoothing
+            if self.normalize_gains:
+                g_mag_med, g_mag_std = get_med_std(np.abs(gain_arr), 100)
+                gain_arr *= tools.invert_no_zero(g_mag_med[..., np.newaxis]) 
 
             # Get the weight array if it's there
             weight_arr = gain.weight[:].view(np.ndarray) if gain.weight is not None else None
