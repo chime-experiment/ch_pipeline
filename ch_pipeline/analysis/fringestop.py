5008dda072ee8069a1587cff4f8b1fdd089c4800
Meiling Deng <mdeng@phas.ubc.ca>
2019-01-18 13:29:34 -0800

    change the frequency

--- ch_pipeline/analysis/fringestop.py
+++ ch_pipeline/analysis/fringestop.py
@@ -42,7 +42,8 @@ class FringeStop(task.SingleTask):
 
         start_freq = tstream.vis.local_offset[0]
         nfreq = tstream.vis.local_shape[0]
-        freq = tstream.freq['centre'][start_freq:start_freq + nfreq]
+        end_freq = start_freq + nfreq
+        freq = tstream.freq['centre'][start_freq:end_freq]
         prod_map = tstream.index_map['prod']
         src = ephemeris.get_source_dictionary(self.source_file)[self.source] 
         dtime = ephemeris.unix_to_datetime(tstream.time[0])
