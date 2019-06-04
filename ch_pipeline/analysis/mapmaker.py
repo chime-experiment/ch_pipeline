fix(CombinedRingMapMaker): Fixed beam normalization

ch_pipeline/analysis/mapmaker.py
@@ -291,7 +291,7 @@ class CombinedRingMapMaker(task.SingleTask):
 
     include_auto = config.Property(proptype=bool, default=False)
 
-    time_window_s = config.Property(proptype=float, default=900.)
+    time_window_s = config.Property(proptype=float, default=300.)
 
     src_name = config.Property(proptype=str, default='CygA')
 
@@ -393,14 +393,16 @@ class CombinedRingMapMaker(task.SingleTask):
                 beam[:, pp] = self._beamfunc(np.radians(ha_w)[np.newaxis, :, np.newaxis], pp, 
                                              f_MHz[:, np.newaxis, np.newaxis], 
                                              np.radians(dec)[np.newaxis, np.newaxis, :])
-        # Normalize beam. Currently the beam is normalized by th sum of beam pixel values 
-        # at the declination of the calibration source. With this normalization, the
-        # flux of a calibration point source at transit time should be correct after map stacking
+        # Normalize beam. Currently the beam is normalized by th sum of the squares of the
+        # beam pixel values at the declination of the calibration source. With this normalization, 
+        # and assuming the true beam, the flux of a point source at the calibration declination
+        # at transit time should be correct after map stacking (note I normalize the beam squared
+        # because the data alreaddy has one factor of the beam in it)
         src_dict = {'CygA': ephemeris.CygA, 'CasA': ephemeris.CasA, 
                     'TauA': ephemeris.TauA, 'VirA': ephemeris.VirA}
         dec_cal = np.rad2deg(src_dict[self.src_name].dec.radians) # Declination of the calibrator
         dec_cal_index = np.argmin(abs(dec-dec_cal)) # Declination index closest to dec_cal
-        beam /= np.sum(beam[:, :, :, dec_cal_index], axis=2)[:, :, np.newaxis, np.newaxis]
+        beam /= np.sum(beam[:, :, :, dec_cal_index]**2, axis=2)[:, :, np.newaxis, np.newaxis]
 
         # Construct weight function.
         # Handle different options for weighting baselines