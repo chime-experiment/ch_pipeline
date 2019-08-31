chime-experiment/ch_pipeline

commit fe21bb7ad5511e718f762e38723581191ba1037e

fix(CalibrationCorrection): allow for multiple `DataFlag` types with same name

ch_pipeline/analysis/calibration.py
@@ -709,8 +709,7 @@ class CalibrationCorrection(task.SingleTask):
                     flags = list(finder.DataFlag.select().where(finder.DataFlag.type == ft))
                     # Only keep flags that will produce nonzero corrections, as defined by
                     # the _correction_is_nonzero method
-                    flags = [flg for flg in flags if self._correction_is_nonzero(**flg.metadata)]
-                    break
+                    flags += [flg for flg in flags if self._correction_is_nonzero(**flg.metadata)]
 
         # Share flags with other nodes
         flags = self.comm.bcast(flags, root=0)
@@ -725,7 +724,7 @@ class CalibrationCorrection(task.SingleTask):
         Parameters
         ----------
         sstream : andata.CorrData, containers.SiderealStream, or equivalent
-            Apply a phase correction to the `vis` dataset in this container.
+            Apply a correction to the `vis` dataset in this container.
         inputmap : list of :class:`CorrInput`
             List describing the inputs as they are in the file, output from
             `tools.get_correlator_inputs()`

commit c59c80f9126f09e082225851fca9a228da95b5d1

fix(CalibrationCorrection): fix bug introduced in commit fe21bb7

ch_pipeline/analysis/calibration.py
@@ -706,10 +706,10 @@ class CalibrationCorrection(task.SingleTask):
             flag_types = finder.DataFlagType.select()
             for ft in flag_types:
                 if ft.name == self.name_of_flag:
-                    flags = list(finder.DataFlag.select().where(finder.DataFlag.type == ft))
+                    ftemp = list(finder.DataFlag.select().where(finder.DataFlag.type == ft))
                     # Only keep flags that will produce nonzero corrections, as defined by
                     # the _correction_is_nonzero method
-                    flags += [flg for flg in flags if self._correction_is_nonzero(**flg.metadata)]
+                    flags += [flg for flg in ftemp if self._correction_is_nonzero(**flg.metadata)]
 
         # Share flags with other nodes
         flags = self.comm.bcast(flags, root=0)