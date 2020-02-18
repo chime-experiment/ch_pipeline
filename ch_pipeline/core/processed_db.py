c82723f1b8fb959c40f756fb7fc4a77d48034c8b
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2020-02-18 15:04:04 -0800

    style(core.processed_db): Format docstrings.

--- ch_pipeline/core/processed_db.py
+++ ch_pipeline/core/processed_db.py
@@ -1,13 +1,13 @@
-""" Database of processed data files
+"""Database of processed data files
 
-    Tasks and functions for interacting with the database of processed data files.
-    For the moment, the "database" is implemented as a YAML file containing a list
-    of files and associated metadata.
+Tasks and functions for interacting with the database of processed data files.
+For the moment, the "database" is implemented as a YAML file containing a list
+of files and associated metadata.
 
-    Tasks
-    =====
+Tasks
+=====
 
-    RegisterProcessedFiles
+RegisterProcessedFiles
 """
 
 from caput import config, mpiutil
@@ -18,15 +18,17 @@ import git
 
 
 class RegisterProcessedFiles(task.SingleTask):
-    """ Register a file in the processed data database. For now this is implemented
-        as a YAML file specified in the config parameters.
+    """Register a file in the processed data database.
 
-        For most cases, this class should be inherited and the process method
-        overridden to save the information relevant to the type of processed data
-        being saved.
+    For now this is implemented as a YAML file specified in the
+    config parameters.
 
-        The 'product_type' parameter is meant to identify the type of processing and
-        the 'tag' could be used to distinguish between runs.
+    For most cases, this class should be inherited and the process method
+    overridden to save the information relevant to the type of processed data
+    being saved.
+
+    The 'product_type' parameter is meant to identify the type of processing and
+    the 'tag' could be used to distinguish between runs.
     """
 
     output_root = config.Property(proptype=str, default=None)
@@ -35,8 +37,8 @@ class RegisterProcessedFiles(task.SingleTask):
     db_fname = config.Property(proptype=str)
 
     def setup(self):
-        """ Extract git tags for a few important pipeline packages: `ch_util`,
-            `caput`, `ch_pipeline`, `draco`, and `driftscan`.
+        """Extract git tags for a few important pipeline packages: `ch_util`,
+        `caput`, `ch_pipeline`, `draco`, and `driftscan`.
         """
 
         # Extract config, git tags
@@ -67,17 +69,17 @@ class RegisterProcessedFiles(task.SingleTask):
         # TODO: figure out how to get access to config
 
     def process(self, output):
-        """ Writes out a file and registers it in the database. Saves the file path,
-            software git tags, product type, and tag. The file name is prefixed with
-            the 'output_root' config parameter.
+        """Writes out a file and registers it in the database. Saves the file path,
+        software git tags, product type, and tag. The file name is prefixed with
+        the 'output_root' config parameter.
 
-            This should be overriden to register information relevant to specific
-            types of processed data if necessary.
+        This should be overriden to register information relevant to specific
+        types of processed data if necessary.
 
-            Parameters
-            ----------
-            output: caput.memh5.MemDiskGroup
-                Data container to save and register.
+        Parameters
+        ----------
+        output: caput.memh5.MemDiskGroup
+            Data container to save and register.
         """
 
         # Create a tag for the output file name
@@ -110,24 +112,24 @@ class RegisterProcessedFiles(task.SingleTask):
 def append_product(
     db_fname, prod_fname, prod_type, config, tag=None, git_tags=[], **kwargs
 ):
-    """ Utility function to append a processed file to the YAML-based database.
+    """Utility function to append a processed file to the YAML-based database.
 
-        Parameters
-        ----------
-        db_fname: str
-            Path to database YAML file.
-        prod_fname: str
-            Path to processed file to register.
-        prod_type: str
-            Type of processing.
-        config: dict
-            The pipeline config used to generate the data.
-        tag: str
-            A tag for this processing run.
-        git_tags: dict
-            Git tags for relevant software packages.
-        **kwargs: dict
-            Any other metadata to register with this file.
+    Parameters
+    ----------
+    db_fname: str
+        Path to database YAML file.
+    prod_fname: str
+        Path to processed file to register.
+    prod_type: str
+        Type of processing.
+    config: dict
+        The pipeline config used to generate the data.
+    tag: str
+        A tag for this processing run.
+    git_tags: dict
+        Git tags for relevant software packages.
+    **kwargs: dict
+        Any other metadata to register with this file.
     """
 
     with open(db_fname, "r") as fh:
