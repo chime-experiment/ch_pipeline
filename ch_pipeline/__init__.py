37270d56fe60ef11984d4536aafb0f4a365626a5
ljgray <liam.j.gray@usask.ca>
2022-07-19 18:35:41 -0700

    feat(init): add lazy versioning

--- ch_pipeline/__init__.py
+++ ch_pipeline/__init__.py
@@ -11,7 +11,15 @@ Submodules
     synthesis
 
 """
-from ._version import get_versions
 
-__version__ = get_versions()["version"]
-del get_versions
+
+def __getattr__(name: str):
+    """Custom handling for __version__ global variable."""
+    if name == "__version__":
+        # No need to re-get __version__ if it already exists
+        v = globals().get(name, None)
+        from ._version import get_versions
+
+        return v if v else get_versions()["version"]
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
