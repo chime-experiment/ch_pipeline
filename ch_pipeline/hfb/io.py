7d0353cb8dc1d1af81c13528ad551795ae09f871
Rik van Lieshout <lieshout@cita.utoronto.ca>
2023-01-31 11:06:33 -0800

    feat(io): add loading HFB data from params

--- ch_pipeline/hfb/io.py
+++ ch_pipeline/hfb/io.py
@@ -17,7 +17,7 @@ from beam_model.formed import FFTFormedActualBeamModel
 from .containers import HFBReader
 
 
-class BaseLoadHFBDataFiles(io.BaseLoadFiles):
+class BaseLoadFiles(io.BaseLoadFiles):
     """Base class for loading CHIME HFB data from files on disk into containers.
 
     Attributes
@@ -122,14 +122,22 @@ class BaseLoadHFBDataFiles(io.BaseLoadFiles):
 
         return beam_index_ns
 
-    def _load_files(self, files, time_range=(None, None)):
-        # Load a file into the HFBData container.
+    def _load_filelist(self, files, time_range=(None, None)):
+        """Load a list of files into the HFBData container.
+
+        Parameters
+        ----------
+        files : list
+            List of filenames to load into container.
+        time_range: tuple
+            Unix timestamps bracketing the part of the data to be loaded.
+        """
 
         for filename in files:
             if not os.path.exists(filename):
                 raise RuntimeError(f"File does not exist: {filename}")
 
-        self.log.info(f"Loading file {files}")
+        self.log.info(f"Loading files {files}")
         self.log.debug(f"Reading with time range: {time_range}")
         self.log.debug(f"Reading with freq selections: {self.freq_sel}")
         self.log.debug(f"Reading with beam selections: {self.beam_sel}")
@@ -146,7 +154,7 @@ class BaseLoadHFBDataFiles(io.BaseLoadFiles):
         # Select beams
         rd.beam_sel = self.beam_sel
 
-        # Read file
+        # Read files
         cont = rd.read()
 
         if self.redistribute is not None:
@@ -155,61 +163,96 @@ class BaseLoadHFBDataFiles(io.BaseLoadFiles):
         return cont
 
 
-class LoadHFBDataFromFilelist(BaseLoadHFBDataFiles):
-    """Load CHIME HFB data from a file list passed into the setup routine."""
+class LoadFilesFromParams(BaseLoadFiles):
+    """Load CHIME HFB data from files given in the task's parameters.
 
-    files = None
+    Attributes
+    ----------
+    filegroups : list or dict
+        A dictionary specifying a file group, or a list of them. In addition to
+        the standard components of file groups ('tag' and 'files'; see documentation
+        in :class:`draco.core.io`), the file groups can also have a 'time_range',
+        given as a list of two unix timestamps. Example YAML content:
 
-    _file_ptr = 0
+    .. code-block:: yaml
 
-    def setup(self, files):
-        """Set the list of files to load; set up frequency and beam selection.
+        filegroups:
+          - tag: '20230108'
+            files: ['/mnt/gong/archive/20221221T181623Z_chime_hfb/hfb_01504956_0000.h5',
+                    '/mnt/gong/archive/20221221T181623Z_chime_hfb/hfb_01510110_0000.h5']
+            time_range: [1673156146.031947, 1673157946.031947]
+          - tag: '20230109'
+            files: ['/mnt/gong/archive/20221221T181623Z_chime_hfb/hfb_01592573_0000.h5']
+            time_range: [1673242310.130873, 1673244110.130873]
+    """
 
-        Parameters
-        ----------
-        files : list
-            List of lists of filenames, or list of tuples, where each tuple
-            consists of a list of filenames and a tuple of time bounds. Each
-            item in the main list will be places in a single HFBData container.
-        """
-        if not isinstance(files, (list, tuple)):
-            raise RuntimeError("Argument must be list of lists of files.")
+    filegroups = config.Property(proptype=io._list_of_filegroups)
 
-        self.files = files
-
-        # Call the baseclass setup to resolve any selections
-        super().setup()
+    _fgroup_ptr = 0
 
     def process(self):
-        """Load in each sidereal day.
+        """Load in each filegroup (e.g., a sidereal day).
 
         Returns
         -------
         ts : HFBData
-            The timestream of each sidereal day.
+            The timestream of each filegroup.
         """
 
-        if len(self.files) == self._file_ptr:
+        if len(self.filegroups) == self._fgroup_ptr:
             raise pipeline.PipelineStopIteration
 
         # Collect garbage to remove any prior data objects
         gc.collect()
 
         # Fetch and remove the first item in the list
-        file_ = self.files[self._file_ptr]
-        self._file_ptr += 1
+        filegroup = self.filegroups[self._fgroup_ptr]
+        self._fgroup_ptr += 1
 
-        # Handle file lists including time ranges
-        if isinstance(file_, tuple):
-            time_range = file_[1]
-            file_ = file_[0]
-        else:
-            time_range = (None, None)
+        if "time_range" not in filegroup:
+            filegroup["time_range"] = (None, None)
 
-        # Read file(s)
-        self.log.info(f"Reading file {self._file_ptr} of {len(self.files)}.")
-
-        ts = self._load_files(file_, time_range)
+        # Read filegroup
+        self.log.info(
+            f"Reading filegroup {self._fgroup_ptr} of {len(self.filegroups)}."
+        )
+        ts = self._load_filelist(filegroup["files"], filegroup["time_range"])
 
         # Return timestream
         return ts
+
+
+class LoadFiles(LoadFilesFromParams):
+    """Load CHIME HFB data from file lists passed into the setup routine."""
+
+    filelists = None
+
+    def setup(self, filelists):
+        """Convert lists of files to list of filegroups; set up frequency and beam selection.
+
+        Parameters
+        ----------
+        filelists : list
+            List of lists of filenames, or list of tuples, where each tuple
+            consists of a list of filenames and a tuple of time bounds. Each
+            item in the main list will be places in a single HFBData container.
+        """
+        if not isinstance(filelists, list):
+            raise RuntimeError("Argument must be list of lists of files.")
+
+        # Convert list of filelists to list of filegroups
+        self.filegroups = []
+        for i, flist in enumerate(filelists):
+
+            tag = f"group_{i}"
+
+            # Handle lists including time ranges
+            if isinstance(flist, tuple):
+                fgroup = {"files": flist[0], "time_range": flist[1], "tag": tag}
+            else:
+                fgroup = {"files": flist, "time_range": (None, None), "tag": tag}
+
+            self.filegroups.append(fgroup)
+
+        # Call the baseclass setup to resolve any selections
+        super().setup()
