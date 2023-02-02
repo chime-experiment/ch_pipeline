d4ddf430e51c7909f7fd56e7d85316848a81a418
Rik van Lieshout <lieshout@cita.utoronto.ca>
2023-02-02 09:05:27 -0800

    feat(hfb): add HFB time average task

---
+++ ch_pipeline/hfb/analysis.py
@@ -0,0 +1,81 @@
+"""Tasks for HFB analysis
+"""
+
+import numpy as np
+
+from caput import config
+
+from draco.core import task
+from draco.util import tools
+
+from . import containers
+
+
+class HFBMakeTimeAverage(task.SingleTask):
+    """Take average over time axis.
+
+    Used for making sub-frequency band shape template and general time averaging.
+
+    Attributes
+    ----------
+    weighting: str (default: "inverse_variance")
+        The weighting to use in the stack.
+        Either `uniform` or `inverse_variance`.
+    """
+
+    weighting = config.enum(["uniform", "inverse_variance"], default="inverse_variance")
+
+    def process(self, stream):
+        """Take average over time axis.
+
+        Parameters
+        ----------
+        stream : HFBData, HFBHighResData
+            Container with HFB data and weights.
+
+        Returns
+        -------
+        out : HFBTimeAverage, HFBHighResTimeAverage
+            Average of stream over time axis.
+        """
+
+        contmap = {
+            containers.HFBData: containers.HFBTimeAverage,
+            containers.HFBHighResData: containers.HFBHighResTimeAverage,
+        }
+
+        # Get the output container
+        out_cont = contmap[stream.__class__]
+
+        if self.weighting == "uniform":
+            # Number of samples with non-zero weight in each time bin
+            nsamples = np.count_nonzero(stream.weight[:], axis=-1)
+
+            # For uniform weighting, the average of the variances is
+            # sum( 1 / weight ) / nsamples,
+            # and the averaged weight is the inverse of that
+            variance = tools.invert_no_zero(stream.weight[:])
+            weight = nsamples / np.sum(variance, axis=-1)
+
+            # For uniform weighting, the averaged data is the average of all
+            # non-zero data
+            data = np.sum(stream.hfb[:], axis=-1) / nsamples
+
+        else:
+            # For inverse-variance weighting, the averaged weight turns out to
+            # be equal to the sum of the weights
+            weight = np.sum(stream.weight[:], axis=-1)
+
+            # For inverse-variance weighting, the averaged data is the weighted
+            # sum of the data, normalized by the sum of the weights
+            data = np.sum(stream.weight[:] * stream.hfb[:], axis=-1) / weight
+
+        # Create container to hold output
+        out = out_cont(axes_from=stream, attrs_from=stream)
+
+        # Save data to output container
+        out.hfb[:] = data
+        out.weight[:] = weight
+
+        # Return output container
+        return out
