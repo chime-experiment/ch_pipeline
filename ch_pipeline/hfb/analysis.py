3fa2bc653257062e40828e6ac711f3260cb6d2bd
Rik van Lieshout <lieshout@cita.utoronto.ca>
2023-08-01 15:49:24 -0400

    fix(hfb): remove redistribute in `HFBStackDays`

--- ch_pipeline/hfb/analysis.py
+++ ch_pipeline/hfb/analysis.py
@@ -330,8 +330,6 @@ class HFBStackDays(task.SingleTask):
                 self.stack.add_dataset("nsample")
                 self.stack.nsample[:] = 0
 
-            self.stack.redistribute("freq")
-
             self.lsd_list = []
 
         # Accumulate
