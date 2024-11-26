58a9fe77d9702e03a525f38751cdcb05bc3a8871
Seth Siegel <ssiegel@cedar1.int.cedar.computecanada.ca>
2024-11-26 07:30:45 -0800

    fix(flagging): correct phase wrapping for bright source track mask

--- ch_pipeline/analysis/flagging.py
+++ ch_pipeline/analysis/flagging.py
@@ -3076,6 +3076,7 @@ class MaskBrightSourceTracks(MaskBrightSourcePixels):
         src_ra, src_dec, src_y = super().get_source_coordinates(ringmap)
 
         ha = np.radians(ringmap.ra[np.newaxis, :] - src_ra[:, np.newaxis])
+        ha = ((ha + np.pi) % (2 * np.pi)) - np.pi  # correct phase wrap
 
         x, y, z = interferometry.sph_to_ground(
             ha, self.latitude, np.radians(src_dec[:, np.newaxis])
